ba6672bb72eea27c4757265e765deecd47abec67
kuchu13 <105021033@live.asia.edu.tw>
2016-11-20 21:42:21 +0800

    1 not yet

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -1,9 +1,21 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw05 {
 
 	public static void main(String[] args) {
-
+		Scanner scn = new Scanner(System.in);
+		int v1 = scn.nextInt();
+		int sum = 0;
+		System.out.print(fun(v1, sum));
 	}
-
+	public static int fun(int v1, int sum) {
+		if (v1 == 0) {
+			return sum;
+		} else {
+			sum = sum * 10;
+			return fun(v1 / 10, sum + v1 % 10);
+		}
+	}
 }
