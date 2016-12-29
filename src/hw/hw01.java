4cbe5f0738ea5d654b557c9cfbe4375d39fbc974
kuchu13 <105021033@live.asia.edu.tw>
2016-12-29 20:27:43 +0800

    week7hw1
    最小公倍

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,13 +1,28 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01 {
 
 	public static void main(String[] args) {
-
 		
-		
-		
-		
+    	Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入三數");
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		int z = scn.nextInt();
+		System.out.println("最小公倍數:"+lcm(lcm(x, y),z));
 	}
-
+	
+	public static int lcm(int v1,int v2){
+		return v1*v2/cm(v1,v2);
+	}
+	
+	public static int cm(int v1,int v2){
+		if(v2 == 0){
+			return v1;
+		}else{
+			return cm(v2, v1%v2);
+		}
+	}
 }
